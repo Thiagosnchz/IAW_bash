7627711c56406c106163d945b16bd5a572aa7c07
andresedal <92717053+andresedal@users.noreply.github.com>
2021-11-17 13:59:19 +0100

    crear usuario v2

--- createuser.sh
+++ createuser.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #Crear usuario
+
 read -p 'Nombre de usuario: ' nombre
 read -p 'Contraseña' password
-echo $nombre 
+echo $nombre
 echo $password
 
-useradd -p $password $nombre
-mkdir /var/www/$nombre/{web,blog,files}
-chown root:root /var/www/$nombre
-chown $nombre:$nombre /var/www/$nombre/*
-chmod -R 755 /var/www/ 
-
-
+if [ -d /var/www/$nombre ]; then
+	echo 'el usuario' $nombre 'existe'
+else
+	useradd -p $password $nombre
+	mkdir /var/www/$nombre/{web,blog,files}
+	chown root:root /var/www/$nombre
+	chown $nombre:$nombre /var/www/$nombre/*
+	chmod -R 755 /var/www/
+fi
 
